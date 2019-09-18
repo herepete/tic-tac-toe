e0e38ec0361d3894ae96657878900b91834bfc6b
herepete <root@linuxblog.info>
2019-09-18 08:46:07 +0000

    Update README.md and Error on incorrect input

--- tic_tac_toe/utils.py
+++ tic_tac_toe/utils.py
@@ -49,7 +49,7 @@ def usr_input(player):
 
             return row, column
         except (ValueError, IndexError):
-            print('Incorrect input. Try again. ')
+            print('Incorrect input. Try again.(Your input needs to be in the format <number><space> <number> i.e "2 2" for the middle of the board) ')
 
 
 def pick_corner(board):
